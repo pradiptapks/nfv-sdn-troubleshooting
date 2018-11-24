1faed4105ed5c14a12fa3ab87626c796e72a3dae
Pradipta Sahoo <pradiptapks@gmail.com>
2018-11-24 11:47:08 -0500

    ovs-trace-1

--- ovs-trace.sh
+++ ovs-trace.sh
@@ -9,6 +9,8 @@ if [ ! `openstack server list | grep $comp_ip | awk '{print $8}'| cut -d \= -f 2
   exit 1
 fi
 
+host_name=`ssh -oStrictHostKeyChecking=no heat-admin@$comp_ip "hostname"`
+
 echo "Enter OVS Bridge name :"; read br_name
 echo "Enter OVS port Number :"; read in_port
 echo "Enter Protocol Details :"; read proto
@@ -17,23 +19,23 @@ echo "Enter MAC Address of Router internal interfae :"; read dl_dst
 echo "Enter IP Address of VM Internal network which associated with FIP :"; read nw_src
 echo "Enter IP Address of External Physical Gateway :"; read nw_dst
 
-echo "--------------------------";
-echo "Open Flow Trace from source port to destination port";
-echo "--------------------------";
+IFS=$'\n' ;
+dir_name=/tmp/ovs-trace
+file=$host_name-ofproto-trace.txt
+rm -f ${dir_name}/*
+mkdir -p ${dir_name}
 
-ssh -oStrictHostKeyChecking=no heat-admin@$comp_ip "sudo ovs-appctl ofproto/trace $br_name "in_port=$in_port,$proto,dl_src=$dl_src,dl_dst=$dl_dst,nw_src=$nw_src,nw_dst=$nw_dst""
+ssh -oStrictHostKeyChecking=no heat-admin@$comp_ip "sudo ovs-appctl ofproto/trace $br_name "in_port=$in_port,$proto,dl_src=$dl_src,dl_dst=$dl_dst,nw_src=$nw_src,nw_dst=$nw_dst"" | tee -a ${dir_name}/$file
 
 
-for i in `ssh -oStrictHostKeyChecking=no heat-admin@$comp_ip "sudo ovs-appctl ofproto/trace $br_name "in_port=$in_port,$proto,dl_src=$dl_src,dl_dst=$dl_dst,nw_src=$nw_src,nw_dst=$nw_dst" | cut -c1-3 | grep [0-9]| cut -d \. -f 1"`
+for i in `ssh -oStrictHostKeyChecking=no heat-admin@$comp_ip "sudo ovs-appctl ofproto/trace $br_name "in_port=$in_port,$proto,dl_src=$dl_src,dl_dst=$dl_dst,nw_src=$nw_src,nw_dst=$nw_dst" | cut -c1-3 | grep [0-9]| cut -d \. -f |tr -d ' '"`
 do
-echo -e "\n============ DUMP FLOW TABLE $i :-"
-ssh -oStrictHostKeyChecking=no heat-admin@$comp_ip "sudo ovs-ofctl -O OpenFlow13 dump-flows $br_name table:$i"
+echo -e "\n # ovs-ofctl -O OpenFlow13 dump-flows $br_name table:$i" | tee -a ${dir_name}/$file
+ssh -oStrictHostKeyChecking=no heat-admin@$comp_ip "sudo ovs-ofctl -O OpenFlow13 dump-flows $br_name table:$i" | tee -a ${dir_name}/$file
 done
 
-
-
-for j in `ssh -oStrictHostKeyChecking=no heat-admin@$comp_ip "sudo ovs-appctl ofproto/trace $br_name "in_port=$in_port,$proto,dl_src=$dl_src,dl_dst=$dl_dst,nw_src=$nw_src,nw_dst=$nw_dst" | cut -c1-3 | grep [0-9]| cut -d \. -f 1"`
+for j in `ssh -oStrictHostKeyChecking=no heat-admin@$comp_ip "sudo ovs-appctl ofproto/trace $br_name "in_port=$in_port,$proto,dl_src=$dl_src,dl_dst=$dl_dst,nw_src=$nw_src,nw_dst=$nw_dst" | cut -c1-3 | grep [0-9]| cut -d \. -f 1 |tr -d ' '"`
 do
-echo -e "\n============ DUMP FLOW GROUP $j :-"
-ssh -oStrictHostKeyChecking=no heat-admin@$comp_ip "sudo ovs-ofctl -O OpenFlow13 dump-groups $br_name table:$j"
+echo -e "\n # ovs-ofctl -O OpenFlow13 dump-groups $br_name table:$j" | tee -a ${dir_name}/$file
+ssh -oStrictHostKeyChecking=no heat-admin@$comp_ip "sudo ovs-ofctl -O OpenFlow13 dump-groups $br_name table:$j" | tee -a ${dir_name}/$file
 done
